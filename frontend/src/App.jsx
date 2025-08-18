7923936e2f31c6aeaf3bd9eae59195607005e354
gMoraes1 <gustavo.correiademoraes2006@gmail.com>
2025-08-18 19:19:32 -0300

    feat: alteração nas importações

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import "./App.css";
 function App() {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState("");
